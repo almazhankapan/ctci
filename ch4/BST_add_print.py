21ba93ab636179709762a7bdb66cf58eecd7baa7
Almazhan Kapan <aa5456@nyu.edu>
2021-07-26 00:36:15 +0800

    add fixes and resolved questions

--- ch4/BST_add_print.py
+++ ch4/BST_add_print.py
@@ -16,7 +16,7 @@ class BST:
             self.preorder_add(self, self.root, node)
     
     def print(self):
-        out=self.inorder_print(self.root, "")
+        out=self.preorder_print(self.root, "")
         print(out[0:(len(out)-1)])
 
     def preorder_add(self, root, node):
@@ -31,37 +31,11 @@ class BST:
             else: 
                 self.preorder_add(root.right, node)
 
-    def inorder_print(self, root, traversal):
+    def preorder_print(self, root, traversal):
         if root!=None: 
-            traversal=self.inorder_print(root.left, traversal)
             traversal+=str(root.value)+"-"
-            traversal=self.inorder_print(root.right, traversal)
+            traversal=self.preorder_print(root.left, traversal)
+            traversal=self.preorder_print(root.right, traversal)
         return traversal
 
-    def buildMinTree(self, array):
-        self.root=self.minTreeHelper(array)
-        #            5
-        #        4       7
-        #    1     3   6    9
 
-    def minTreeHelper(self, array): 
-        mid=int(len(array)/2)#3 #1 #0
-        #important!!make sure to create the node! 
-        root=self.Node(array[mid])#5 #4 # #1
-        if mid==0:
-            return root #return the element if mid=0
-        root.left=self.minTreeHelper(array[0:mid]) #1
-        root.right=self.minTreeHelper(array[(mid+1): (len(array))])
-        return root
-    
-
-def main():
-    t=BST()
-    t.buildMinTree([1,2,3,4,5,6,7])
-    t.print()
-
-
-
-if __name__=="__main__":
-    main()
-    
--- ch4/ex1.py
+++ ch4/ex1.py
@@ -1,3 +1,31 @@
+class Queue:
+    class NodeQueue: 
+        def __init__(self,value, next):
+            self.value=value
+            self.next=next
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    def enqueue(self, value):
+        newN=self.NodeQueue(value,None )
+        if self.tail==None: 
+            self.tail=newN
+            self.head=newN
+        else: 
+            self.tail.next=newN
+            self.tail=newN
+    def dequeue(self):
+        if self.head==None: 
+            self.tail=None
+            return None
+        else: 
+            v=self.head.value
+            self.head=self.head.next
+            if self.head==None: 
+                self.tail=None
+            return v
+    def isEmpty(self):
+        return self.head==None
 
 class Graph: 
     class Node: 
@@ -5,83 +33,47 @@ class Graph:
             self.value=value
             self.children=[]
             self.visited=False
-
-        def add(self, node):
-            self.children.append(node)
-    class Queue: 
-        class NodeQueue: 
-            def __init__(self, value, next):
-                self.value=value
-                self.next=next
-        
-        def __init__(self):
-            self.head=None
-            self.tail=None
-
-        def enqueue(self, value):
-            newN=self.NodeQueue(value, None)
-            if self.tail==None: 
-                self.tail=newN
-                self.head=newN
-            else: 
-                self.tail.next=newN
-                self.tail=newN
-        
-        def dequeue(self):
-            if self.head==None: 
-                return None
-            else: 
-                v=self.head.value
-                self.head=self.head.next
-                if self.head==None: 
-                    self.tail=None
-                return v
-        
-        def isEmpty(self):
-            return self.head==None
-
+        def add(self, child):
+            self.children.append(child)
     def __init__(self):
         self.nodes=[]
-    
     def add(self, node):
         self.nodes.append(node)
     
-    def findRoutes(self, node1,node2):
-        q=self.Queue()
-        #node1,node2--children 
+    def isRoute(self, node1,node2):
+        #breadth first search
+        q=Queue()
         q.enqueue(node1)
         node1.visited=True
-        while (not q.isEmpty()):
-            r=q.dequeue()#get value-which is Node
-            if r==node2:
+        while(not q.isEmpty()):
+            r=q.dequeue()
+            if r.value==node2.value:
                 return True
-            for x in r.children: 
-                if x.visited==False: 
-                    q.enqueue(x)
-                    x.visited=True
+            else: 
+                for child in r.children: 
+                    if child.visited==False: 
+                        q.enqueue(child)
+                        child.visited=True
         return False
-
 
 def main():
     g=Graph()
-    node1=g.Node(12)
-    node2=g.Node(13)
-    node3=g.Node(17)
-    node4=g.Node(5)
-    node5=g.Node(9)
-    nodeLonely=g.Node(88)
-    node1.add(node2)
+    node1=g.Node(1)
+    node2=g.Node(2)
+    node3=g.Node(3)
+    node4=g.Node(4)
+    node5=g.Node(5)
+    node6=g.Node(6)
+    node3.children=[node2,node4,node5]
     node1.add(node3)
-    node3.add(node4)
-    node4.add(node5)
-    g.nodes=[node1,node2,node3,node4,node5,nodeLonely]
-    print(g.findRoutes(node1,node2))
-    print(g.findRoutes(node1,nodeLonely))
-
+    print(g.isRoute(node1,node3))
+    print(g.isRoute(node1,node6))
 
 if __name__=="__main__":
     main()
 
-       
 
-    
+        
+        
+
+            
